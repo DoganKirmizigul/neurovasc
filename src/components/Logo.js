fc4f3582585d2dbac4d7192445f1bc464b35be89
Doğan Kırmızıgül <dogankirmizigul@outlook.com>
2025-01-22 13:55:40 +0300

    logo added to navbar

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,21 @@
 import React from "react";
+import logo from "../assets/images/logo.png";
 
 function Logo() {
   return (
-    <div className="logo-container">
+    <div
+      className="logo-container"
+      style={{ display: "flex", alignItems: "center", gap: "8px" }}
+    >
+      <img
+        src={logo}
+        alt="Neurovasc Logo"
+        style={{ height: "80px", width: "auto" }} // Logonun boyutunu artırdık
+      />
       <svg
-        width="200"
+        width="200" // SVG genişliğini artırdık
         height="50"
-        viewBox="0 0 200 50"
+        viewBox="0 0 250 50" // Görünüm kutusunu güncelledik
         xmlns="http://www.w3.org/2000/svg"
       >
         <text
@@ -14,7 +23,7 @@ function Logo() {
           y="35"
           fill="#e63946"
           fontFamily="Arial"
-          fontSize="32"
+          fontSize="40" // Yazı boyutunu artırdık
           fontWeight="bold"
         >
           Neurovasc
